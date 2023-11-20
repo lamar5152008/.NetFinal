e547e65d2ffb19de9760a3b58746c246b4dcc65d
Awaab Elamin <AwaabElamin@gmail.com>
2023-11-20 10:08:44 -0600

    test

--- WPFPresntationLayer/obj/Debug/net6.0-windows/WPFPresntationLayer.AssemblyInfo.cs
+++ WPFPresntationLayer/obj/Debug/net6.0-windows/WPFPresntationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFPresntationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a021a12453a685b1e782dcc961920c86305835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFPresntationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFPresntationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
